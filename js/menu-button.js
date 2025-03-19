bbac4782b82144f1c667726db9e2ce78415062fb
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2025-03-19 19:02:31 +0200

    mibile-menu

--- js/menu-button.js
+++ js/menu-button.js
@@ -1,5 +1,24 @@
-const burger = document.querySelector('.burger');
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.mobile-menu');
+  const backdrop = document.querySelector('.menu-backdrop');
+
+
+  function toggleMenu() {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    backdrop.classList.toggle('active');
+  }
+
+
+  burger.addEventListener('click', toggleMenu);
+
+ 
+  backdrop.addEventListener('click', event => {
+    if (event.target === backdrop) {
+      toggleMenu();
+    }
+  });
 });
