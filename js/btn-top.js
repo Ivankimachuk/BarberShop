c101c5895f3b0ee60a0c0736b5b843a85a0a98d7
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2025-03-31 18:48:19 +0300

    added the Up button and completed the adaptation of the booking section

---
+++ js/btn-top.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.back-to-top').click(function () {
+    $('body,html').animate({ scrollTop: 0 }, 800);
+  });
+
+  $(window).scroll(function () {
+    let scrolled = $(window).scrollTop();
+
+    if (scrolled > 350) {
+      $('.back-to-top').addClass('active');
+    } else {
+      $('.back-to-top').removeClass('active');
+    }
+  });
+});
