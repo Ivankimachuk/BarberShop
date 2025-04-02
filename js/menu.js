e678ffe785b432db9e221accce6b24ba1ade36e3
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2025-04-02 19:14:55 +0300

    adaptation made for modal window

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,7 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.mobile-menu');
   const backdrop = document.querySelector('.menu-backdrop');
-
 
   function toggleMenu() {
     burger.classList.toggle('active');
@@ -13,10 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.toggle('no-scroll');
   }
 
-
   burger.addEventListener('click', toggleMenu);
 
- 
   backdrop.addEventListener('click', event => {
     if (event.target === backdrop) {
       toggleMenu();
--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,30 @@
-(() => {
-  const refs = {
-    openModalBtns: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+document.addEventListener('DOMContentLoaded', function () {
+  const openModalBtns = document.querySelectorAll('[data-modal-open]');
+  const closeModalBtn = document.querySelector('[data-modal-close]');
+  const modal = document.querySelector('[data-modal]');
+  const backdrop = document.querySelector('.backdrop');
 
-  refs.openModalBtns.forEach(button => {
+  function toggleModal() {
+    modal.classList.toggle('is-hidden');
+
+    document.body.classList.toggle('no-scroll');
+  }
+
+  openModalBtns.forEach(button => {
     button.addEventListener('click', toggleModal);
   });
 
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+  backdrop.addEventListener('click', event => {
+    if (event.target === backdrop) {
+      toggleModal();
+    }
+  });
+
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape' && !modal.classList.contains('is-hidden')) {
+      toggleModal();
+    }
+  });
+});
